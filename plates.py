165035d11cca7a1ceddf4d805dafa995572edd77
duanniten <duann_@hotmail.com>
2024-09-19 19:44:21 -0300

    Change order of if

--- plates.py
+++ plates.py
@@ -7,14 +7,15 @@ def main():
 
 
 def is_valid(s:str):
-    #All vanity plates must start with at least two letters
-    if not s[0].isalpha() or not s[1].isalpha():
-        return False
-    
+      
     #vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.
     if not 1<len(s)<7:
         return False
     
+    #All vanity plates must start with at least two letters
+    if not s[0].isalpha() or not s[1].isalpha():
+        return False
+
     #Numbers cannot be used in the middle of a plate
     first_number = False
     for l in s:
